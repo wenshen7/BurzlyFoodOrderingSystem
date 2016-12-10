a83d7920cd54246deb850b0463c9a910af596136
leonzly <leonlai0404@gmail.com>
2016-12-10 21:18:26 +0800

    [Finishes #135512963] Done Supplier Information

---
+++ Burzly- search supplier/Burzly/FrmSupplierGrids.Designer.vb
@@ -0,0 +1,101 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class FrmSupplierGrids
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(FrmSupplierGrids))
+        Me.dgvSupplier = New System.Windows.Forms.DataGridView()
+        Me.lblCount = New System.Windows.Forms.Label()
+        Me.txtSearch = New System.Windows.Forms.TextBox()
+        Me.lblsearch = New System.Windows.Forms.Label()
+        CType(Me.dgvSupplier, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'dgvSupplier
+        '
+        Me.dgvSupplier.AllowUserToAddRows = False
+        Me.dgvSupplier.AllowUserToDeleteRows = False
+        Me.dgvSupplier.BackgroundColor = System.Drawing.SystemColors.ActiveCaption
+        Me.dgvSupplier.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dgvSupplier.GridColor = System.Drawing.SystemColors.HotTrack
+        Me.dgvSupplier.Location = New System.Drawing.Point(38, 83)
+        Me.dgvSupplier.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.dgvSupplier.Name = "dgvSupplier"
+        Me.dgvSupplier.ReadOnly = True
+        Me.dgvSupplier.Size = New System.Drawing.Size(1527, 562)
+        Me.dgvSupplier.TabIndex = 9
+        '
+        'lblCount
+        '
+        Me.lblCount.AutoSize = True
+        Me.lblCount.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblCount.Location = New System.Drawing.Point(32, 666)
+        Me.lblCount.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.lblCount.Name = "lblCount"
+        Me.lblCount.Size = New System.Drawing.Size(191, 29)
+        Me.lblCount.TabIndex = 8
+        Me.lblCount.Text = "0 of 0 record(s)"
+        '
+        'txtSearch
+        '
+        Me.txtSearch.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtSearch.Location = New System.Drawing.Point(201, 22)
+        Me.txtSearch.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.txtSearch.Name = "txtSearch"
+        Me.txtSearch.Size = New System.Drawing.Size(334, 35)
+        Me.txtSearch.TabIndex = 1
+        '
+        'lblsearch
+        '
+        Me.lblsearch.AutoSize = True
+        Me.lblsearch.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblsearch.Location = New System.Drawing.Point(92, 26)
+        Me.lblsearch.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.lblsearch.Name = "lblsearch"
+        Me.lblsearch.Size = New System.Drawing.Size(116, 29)
+        Me.lblsearch.TabIndex = 6
+        Me.lblsearch.Text = "&Search : "
+        '
+        'FrmSupplierGrids
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1598, 717)
+        Me.Controls.Add(Me.dgvSupplier)
+        Me.Controls.Add(Me.lblCount)
+        Me.Controls.Add(Me.txtSearch)
+        Me.Controls.Add(Me.lblsearch)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.Name = "FrmSupplierGrids"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Supplier Details"
+        CType(Me.dgvSupplier, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents dgvSupplier As System.Windows.Forms.DataGridView
+    Friend WithEvents lblCount As System.Windows.Forms.Label
+    Friend WithEvents txtSearch As System.Windows.Forms.TextBox
+    Friend WithEvents lblsearch As System.Windows.Forms.Label
+End Class
