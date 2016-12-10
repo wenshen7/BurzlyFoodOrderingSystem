4b4d754ce63d15f26c9c35db859944053d63577d
Kezin <kez_siew@hotmail.com>
2016-12-10 20:39:59 +0800

    [#135512841] search stock code

---
+++ Burzly-Stock Information/Burzly/FrmStockGrid.Designer.vb
@@ -0,0 +1,96 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class FrmStockGrid
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(FrmStockGrid))
+        Me.txtSearch = New System.Windows.Forms.TextBox()
+        Me.lblSearch = New System.Windows.Forms.Label()
+        Me.lblCount = New System.Windows.Forms.Label()
+        Me.dgvStock = New System.Windows.Forms.DataGridView()
+        CType(Me.dgvStock, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'txtSearch
+        '
+        Me.txtSearch.Font = New System.Drawing.Font("Rockwell Condensed", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtSearch.Location = New System.Drawing.Point(114, 28)
+        Me.txtSearch.Name = "txtSearch"
+        Me.txtSearch.Size = New System.Drawing.Size(162, 26)
+        Me.txtSearch.TabIndex = 1
+        '
+        'lblSearch
+        '
+        Me.lblSearch.AutoSize = True
+        Me.lblSearch.Font = New System.Drawing.Font("Rockwell Condensed", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblSearch.Location = New System.Drawing.Point(39, 30)
+        Me.lblSearch.Name = "lblSearch"
+        Me.lblSearch.Size = New System.Drawing.Size(67, 19)
+        Me.lblSearch.TabIndex = 7
+        Me.lblSearch.Text = "Search : "
+        '
+        'lblCount
+        '
+        Me.lblCount.AutoSize = True
+        Me.lblCount.Font = New System.Drawing.Font("Rockwell Condensed", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblCount.Location = New System.Drawing.Point(30, 405)
+        Me.lblCount.Name = "lblCount"
+        Me.lblCount.Size = New System.Drawing.Size(80, 19)
+        Me.lblCount.TabIndex = 6
+        Me.lblCount.Text = "0 Record(s)"
+        '
+        'dgvStock
+        '
+        Me.dgvStock.AllowUserToAddRows = False
+        Me.dgvStock.AllowUserToDeleteRows = False
+        Me.dgvStock.BackgroundColor = System.Drawing.SystemColors.ActiveCaption
+        Me.dgvStock.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.dgvStock.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dgvStock.GridColor = System.Drawing.SystemColors.HotTrack
+        Me.dgvStock.Location = New System.Drawing.Point(30, 83)
+        Me.dgvStock.Name = "dgvStock"
+        Me.dgvStock.ReadOnly = True
+        Me.dgvStock.Size = New System.Drawing.Size(1057, 303)
+        Me.dgvStock.TabIndex = 5
+        '
+        'FrmStockGrid
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1116, 452)
+        Me.Controls.Add(Me.txtSearch)
+        Me.Controls.Add(Me.lblSearch)
+        Me.Controls.Add(Me.lblCount)
+        Me.Controls.Add(Me.dgvStock)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "FrmStockGrid"
+        Me.Text = "Stock Details"
+        CType(Me.dgvStock, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents txtSearch As System.Windows.Forms.TextBox
+    Friend WithEvents lblSearch As System.Windows.Forms.Label
+    Friend WithEvents lblCount As System.Windows.Forms.Label
+    Friend WithEvents dgvStock As System.Windows.Forms.DataGridView
+End Class
---
+++ Burzly-Stock Information/Burzly/FrmStockGrid.vb
@@ -0,0 +1,73 @@
+﻿Imports System.Drawing.Imaging
+Public Class FrmStockGrid
+    Private db As BurzlyDataContext
+    Friend supplierID As String
+
+    Public Sub BindData()
+        Dim id As String = txtSearch.Text
+        Dim st = From stk In db.Stocks Where stk.stkID.Contains(id)
+
+        dgvStock.DataSource = From ds In db.Stocks
+                         Select New With {
+                           ds.stkID,
+                           ds.stkType,
+                           ds.stkName,
+                           ds.stkDescription,
+                           ds.stkBrand,
+                           ds.stkBeforeGst,
+                           ds.stkAfterGst,
+                           ds.stkStatus,
+                           ds.stkQty,
+                           ds.supID}
+
+        dgvStock.Columns("stkID").HeaderText = "Stock ID"
+        dgvStock.Columns("stkType").HeaderText = "Stock Type"
+        dgvStock.Columns("stkName").HeaderText = "Stock Name"
+        dgvStock.Columns("stkDescription").HeaderText = "Stock Description"
+        dgvStock.Columns("stkBrand").HeaderText = "Stock Brand"
+        dgvStock.Columns("stkBeforeGst").HeaderText = "Stock Price Before GST"
+        dgvStock.Columns("stkAfterGst").HeaderText = "Stock Price After GST"
+        dgvStock.Columns("stkStatus").HeaderText = "Stock Status"
+        dgvStock.Columns("stkQty").HeaderText = "Stock Quantity"
+        dgvStock.Columns("supID").HeaderText = "Supplier ID"
+
+        lblCount.Text = st.Count().ToString("0 record(s)")
+
+    End Sub
+
+    Private Sub FrmStockGrid_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'TODO: This line of code loads data into the 'BurzlyDBDataSet.Supplier' table. You can move, or remove it, as needed.
+        db = New BurzlyDataContext()
+        dgvStock.DataSource = db.Stocks
+        BindData()
+        txtSearch.Text = ""
+    End Sub
+
+
+    Private Sub dgvstock_CellContentClick(sender As Object, e As DataGridViewCellEventArgs) Handles dgvStock.CellContentClick
+        Dim i As Integer = e.RowIndex
+    End Sub
+
+    Private Sub btnstock_Click(sender As Object, e As EventArgs)
+
+
+    End Sub
+
+    Private Sub SeacrhData()
+
+        Dim db As New BurzlyDataContext()
+        Dim rs = From o In db.Stocks
+                 Where o.stkID.Contains(txtSearch.Text) Or o.stkName.Contains(txtSearch.Text)
+        dgvStock.DataSource = rs
+
+    End Sub
+
+    Private Sub txtSearch_TextChanged(sender As Object, e As EventArgs) Handles txtSearch.TextChanged
+        SeacrhData()
+    End Sub
+
+    Private Sub btnBack_Click(sender As Object, e As EventArgs)
+        Me.Close()
+
+    End Sub
+End Class
